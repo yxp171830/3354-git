e6f1a71d9ded7e76396928a036f4f5c7299d7a6a
yxp171830 <71400135+nicole619@users.noreply.github.com>
2020-09-20 14:23:06 -0500

    Updated Main.java file
    Modified the addArguments method to add more than two integers and the error message in the main method that reflect the change.

--- Adder/src/adder/Main.java
+++ Adder/src/adder/Main.java
@@ -7,11 +7,16 @@ public class Main {
             int result = addArguments(args);
             System.out.println(result);
         } catch (Exception e) {
-            System.err.println("Please provide two integers to add");
+            System.err.println("Please provide integers to add");   
         }
     }
 
-    private static int addArguments(String[] args) {
-        return Integer.valueOf(args[0]) + Integer.valueOf(args[1]);
+    private static int addArguments(String[] args) throws Exception {
+        if(args.length == 0) throw new Exception();
+		 int sum = 0;
+		 for(int i = 0; i < args.length; i++)
+			  sum = sum + Integer.valueOf(args[i]);
+
+        return sum;
     }
 }
